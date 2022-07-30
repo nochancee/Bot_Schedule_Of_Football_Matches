09da888c6e56939440e8a30742e7adc629ed1030
nochancee <andreyqq19991999@yandex.ru>
2022-07-30 13:03:27 +0300

    minor changes

--- bot.py
+++ bot.py
@@ -101,7 +101,7 @@ async def Raspisanie_Tomorrow(message: types.Message):
     raspisanie_team1_tomorrow = []
     raspisanie_team2_tomorrow = []
     raspisanie_tomorrow = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
-    while i < 50:
+    while i < 51:
         if footbal_schedule_tomorrow[i].text in (clubs) or footbal_schedule_tomorrow[i+1].text in (clubs):
             datatime_tomorrow.append(match_datatime_tomorrow[b].text)
             raspisanie_team1_tomorrow.append(footbal_schedule_tomorrow[i].text)
